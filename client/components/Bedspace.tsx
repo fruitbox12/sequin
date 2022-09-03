e35800983b0f0ba6f9d05418c0b0f306d3d63bb1
codebydolapo <bashorun115@gmail.com>
2022-09-03 16:52:28 -0700

    added dataset

--- client/components/Bedspace.tsx
+++ client/components/Bedspace.tsx
@@ -1,47 +1,4 @@
-// {[
-//     {
-//       path: 'https://ipfs.moralis.io:2053/ipfs/QmS3gdXVcjM72JSGH82ZEvu4D7nS6sYhbi5YyCw8u8z4pE/media/2'
-//     },
-//     {
-//       path: 'https://ipfs.moralis.io:2053/ipfs/QmS3gdXVcjM72JSGH82ZEvu4D7nS6sYhbi5YyCw8u8z4pE/media/0'
-//     },
-//     {
-//       path: 'https://ipfs.moralis.io:2053/ipfs/QmS3gdXVcjM72JSGH82ZEvu4D7nS6sYhbi5YyCw8u8z4pE/media/1'
-//     },
-//     {
-//       path: 'https://ipfs.moralis.io:2053/ipfs/QmS3gdXVcjM72JSGH82ZEvu4D7nS6sYhbi5YyCw8u8z4pE/media/3'
-//     },
-//     {
-//       path: 'https://ipfs.moralis.io:2053/ipfs/QmS3gdXVcjM72JSGH82ZEvu4D7nS6sYhbi5YyCw8u8z4pE/media/4'
-//     },
-//     {
-//       path: 'https://ipfs.moralis.io:2053/ipfs/QmS3gdXVcjM72JSGH82ZEvu4D7nS6sYhbi5YyCw8u8z4pE/media/7'
-//     },
-//     {
-//       path: 'https://ipfs.moralis.io:2053/ipfs/QmS3gdXVcjM72JSGH82ZEvu4D7nS6sYhbi5YyCw8u8z4pE/media/5'
-//     },
-//     {
-//       path: 'https://ipfs.moralis.io:2053/ipfs/QmS3gdXVcjM72JSGH82ZEvu4D7nS6sYhbi5YyCw8u8z4pE/media/6'
-//     }
-//   ]}
-
-
-
-//   [
-//     {
-//         city: "New York",
-//         unoDescription: "3 Guests • 2 Beds • 2 Rooms",
-//         dosDescription: "Wifi • Kitchen • Living Area",
-//         imgUrl:
-//           "https://ipfs.moralis.io:2053/ipfs/QmS3gdXVcjM72JSGH82ZEvu4D7nS6sYhbi5YyCw8u8z4pE/media/3",
-//         lat: "40.716862",
-//         long: "-73.999005",
-//         name: "Apartment in China Town",
-//         pricePerDay: "3",
-//       },
-//   ];
-
-import { Button } from "web3uikit"
+//import { Button } from "web3uikit"
 import styles from '../styles/bedspace.module.css'
 
 interface Props{
@@ -56,14 +13,10 @@ interface Props{
 }
 
 function Bedspace({city, unoDescription, dosDescription, imgUrl, lat, long, name, pricePerDay}: Props) {
-
-  
-
-  // const city = city
-  // const lat = lat
-  // cosnt long = long
-
   return (
+
+
+
     <div className={`max-w-[85%] w-[50rem] min-h-[11rem] h-[11rem] rounded-xl flex flex-row my-5 border-[1px] border-grey ${styles.bedspace}`}>
       <div className={`w-[45%] h-full border-2 border-grey flex justify-center align-center rounded-xl`}>
         <img className={`w-full h-auto rounded-xl`} alt='' src={imgUrl} />
