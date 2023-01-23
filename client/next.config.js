5b346c417685f157d1500dc44fd770feae50c153
fruitbox12 <dylanwong007@gmail.com>
2023-01-23 10:14:36 -0800

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -2,8 +2,20 @@ const { defaultConfig } = require('next/dist/server/config-shared')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
+   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
 
 // module.exports = nextConfig
